31b46714ec50def76be02359eaaa0cc6b2186558
Aaryan Sharma <aaryanradhe8@gmail.com>
2025-04-24 11:19:39 +0530

    some changes

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectMongo = (URL) => {
-    return mongoose.connect(URL)
+    return mongoose.connect(`${URL}/Uber`)
     .then(() => console.log('Mongo Connected!'))
     .catch((error) => console.log(error));
 }
